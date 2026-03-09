607f63c266248a0ee47ae3e6f43d612de5022b83
Heymesson_AZ <hzevedocon.tra21@gmail.com>
2026-03-09 11:02:41 +0000

    Commit de Layout do Visitante e Menu Principal versao 1.0

--- resources/views/components/layouts/visitante.blade.php
+++ resources/views/components/layouts/visitante.blade.php
@@ -9,10 +9,26 @@
 </head>
 
 <body>
+    <nav class="bg-gray-800 text-white p-1" style="display: flex; align-items: center; justify-content: space-between;">
+        <div>
+            <h1 class="text-2xl font-bold">Minha Livraria</h1>
+        </div>
+
+        <div class="search-box">
+            <i data-lucide="search"></i>
+            <input type="text" placeholder="Buscar livros...">
+        </div>
+        
+    </nav>
     <x-menu />
     <main>
         {{ $slot }}
     </main>
 </body>
+<script src="https://unpkg.com/lucide@latest"></script>
+<script>
+    // Ativa todos os ícones do Lucide na página
+    lucide.createIcons();
+</script>
 
 </html>
--- resources/views/components/menu.blade.php
+++ resources/views/components/menu.blade.php
@@ -4,18 +4,76 @@
 		<div class="line"></div>
 		<div class="line"></div>
 	</div>
+	<!-- Icones usando HeroIcons -->
 	<div class="menu-inner">
+		<div class="search-box">
+			<i data-lucide="search"></i>
+			<input type="text" placeholder="Buscar livros...">
+		</div>
 		<ul>
-			<li>Menu Item</li>
-			<li>Menu Item</li>
-			<li>Menu Item</li>
-			<li>Menu Item</li>
-			<li>Menu Item</li>
-			<li>Menu Item</li>
+			<li>
+				<i data-lucide="home"></i>
+				<span>Inicio</span>
+			</li>
+			<li>
+				<i data-lucide="book-open"></i>
+				<span>Catalogo</span>
+			</li>
+			<li>
+				<i data-lucide="tag"></i>
+				<span>Categorias</span>
+				<ul>
+					<li>Ficção</li>
+					<li>Aventura</li>
+					<li>Terror</li>
+					<li>Romance</li>
+					<li>Fantasia</li>
+					<li>Infantil</li>
+					<li>Biografia</li>
+					<li>Autoajuda</li>
+					<li>História</li>
+					<li>Ciência</li>
+					<li>Religião</li>
+					<li>Manga</li>
+				</ul>
+			</li>
+			<li>
+				<i data-lucide="tag"></i>
+				<span>Promoções</span>
+			</li>
+			<li> <i data-lucide="shopping-cart"></i>
+				<span>Carrinho</span>
+			</li>
+			<li>
+				<i data-lucide="user"></i>
+				<span>Conta</span>
+				<ul>
+					<li>
+						<i data-lucide="user"></i>
+						<span>Login</span>
+					</li>
+					<li><i data-lucide="user-plus"></i>
+						<span>Criar Conta</span>
+					</li>
+				</ul>
+			</li>
+			<li> <i data-lucide="contact"></i>
+				<span>Contato</span>
+			</li>
 		</ul>
+		<div class="menu-footer">
+			<!-- ano em tempo real -->
+			<p></p>
+			<div class="social-icons">
+				<i data-lucide="instagram"></i>
+				<i data-lucide="facebook"></i>
+				<i data-lucide="twitter"></i>
+			</div>
+
+		</div>
 	</div>
 
-	<svg version="1.1" id="blob" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+	<svg version="1.1" id="blob">
 		<path id="blob-path" d="M60,500H0V0h60c0,0,20,172,20,250S60,900,60,500z" />
 	</svg>
 </div>

915d80cdab9afebe41370fedb986aa267bf3c911
Heymesson Azevêdo <heymessonazevedo@gmail.com>
2026-09-08 01:24:25 +0000

    feat(mail): configurar envio de emails via Gmail SMTP para notificacoes e recuperacao

--- config/mail.php
+++ config/mail.php
@@ -14,7 +14,7 @@ return [
     |
     */
 
-    'default' => env('MAIL_MAILER', 'log'),
+    'default' => env('MAIL_MAILER', 'smtp'),
 
     /*
     |--------------------------------------------------------------------------
@@ -41,10 +41,10 @@ return [
             'transport' => 'smtp',
             'scheme' => env('MAIL_SCHEME'),
             'url' => env('MAIL_URL'),
-            'host' => env('MAIL_HOST', '127.0.0.1'),
-            'port' => env('MAIL_PORT', 2525),
-            'username' => env('MAIL_USERNAME'),
-            'password' => env('MAIL_PASSWORD'),
+            'host' => env('MAIL_HOST', 'smtp.gmail.com'),
+            'port' => env('MAIL_PORT', 587),
+            'username' => env('MAIL_USERNAME', 'universopapel.notification@gmail.com'),
+            'password' => env('MAIL_PASSWORD', 'czvjxyykmekvyokq'),
             'timeout' => null,
             'local_domain' => env('MAIL_EHLO_DOMAIN', parse_url((string) env('APP_URL', 'http://localhost'), PHP_URL_HOST)),
         ],
@@ -111,8 +111,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'universopapel.notification@gmail.com'),
+        'name' => env('MAIL_FROM_NAME', 'Universo de Papel'),
     ],
 
 ];
